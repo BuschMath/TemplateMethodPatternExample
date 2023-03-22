ace6fba32714307ea87b5b4ebcf0d6144b9c188d
BuschMath <busch@iowacentral.edu>
2023-03-22 14:12:06 -0500

    first commit

---
+++ Game.java
@@ -0,0 +1,11 @@
+abstract class Game {
+    abstract void initialize();
+    abstract void startPlay();
+    abstract void endPlay();
+
+    public final void play() {
+        initialize();
+        startPlay();
+        endPlay();
+    }
+}
